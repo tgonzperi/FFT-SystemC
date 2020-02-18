ec0c41138dc8392013d2ceeb8d91a496b9cdbf8d
tomas <tgonzperi@gmail.com>
2020-02-18 09:45:23 +0100

    Completed FFT8 Modules and Stimuli

--- FFT8.cpp
+++ FFT8.cpp
@@ -2,4 +2,25 @@
 
 
 void FFT8::comportement(){
+  float aux[16];
+  int i=0;
+  while(true){
+    while(i<16){
+      while(in_data.num_available()>0){
+        aux[i] = in_data.read();
+        if(i++ == 16)
+          break;
+      }
+      wait();
+    }
+    i = 0;
+    while(i<16){
+      while(out_data.num_free()>0){
+        out_data.write(aux[i]);
+        if(i++ == 16)
+          break;
+      }
+      wait();
+    }
+  }
 }
---
+++ FiFo_Stimuli.h
@@ -0,0 +1,8 @@
+#include <systemc.h>
+#ifndef FIFO_STIMULI_H
+#define FIFO_STIMULI_H
+
+SC_MODULE(FIFO_stimuli){
+  public:
+    sc_in_clk clk;
+}
