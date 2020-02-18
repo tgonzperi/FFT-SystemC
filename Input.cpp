cd6c1d6fd08dac3969b2b62c066826a5480f2c2a
tomas <tgonzperi@gmail.com>
2020-02-18 08:29:22 +0100

    FiFo Input Module Finished

--- Input.cpp
+++ Input.cpp
@@ -11,18 +11,21 @@ void INPUT::COMPORTEMENT(){
   float tmp_val1, tmp_val2;
 
   if(!fileStream)
-    cerr<<"The file is not opened";
+    cerr<<"The file is not opened"<<endl;
 
   wait();
 
   while(true){
     if(!fileStream.eof()){
-      fileStream >> tmp_val1 >> tmp_val2;
-      out.write(tmp_val1);
-      out.write(tmp_val2);
-      wait();
-    }
-    else{
+      if(oul.num_free()>0){
+        fileStream >> tmp_val1 >> tmp_val2;
+        out.write(tmp_val1);
+        out.write(tmp_val2);
+        wait();
+      }else{
+        cerr << "No space available" << endl;
+      }
+    }else{
       cout << "End of File"; << endl;
       wait();
     }
